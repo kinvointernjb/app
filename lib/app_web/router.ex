bc5f85fd54803a74c8d3bcd029da909082995ebd
kinvointernjb <kinvo.intern.jb@gmail.com>
2018-03-21 16:32:42 +0800

    Fix routes

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -9,16 +9,10 @@ defmodule AppWeb.Router do
     pipe_through :api
 
     resources "/users", UserController, except: [:new, :edit]
-    get "/users/:username", UserController, :show
-    get "/profile/:username", UserController, :profile
     post "/auth", UserController, :authenticate
-    get "/home", PostController, :home
+    resources "/posts", PostController, except: [:new, :edit] do
+      resources "/comments", CommentController, except: [:new, :edit]
+    end
 
-
-    resources "/posts", PostController, except: [:new, :edit]
-
-    resources "/comments", CommentController, except: [:new, :edit]
-    post "/posts/:post_id/comments", CommentController, :create
-    #get "/posts/:post_id/comments", CommentController, :create
   end
 end
